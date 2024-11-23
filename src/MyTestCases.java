2b50aa815f1477e9bb09ebc9d51e3d15e7e535c1
mais-jamil <maisjamil90@gmail.com>
2024-11-23 03:42:15 +0300

    Update

--- src/MyTestCases.java
+++ src/MyTestCases.java
@@ -20,12 +20,56 @@ public class MyTestCases {
 	@BeforeTest
 	public void mySetUp() throws SQLException {
 		
-		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodles","root","mais");
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels","root","mais");
 		
 	}
 	
-	@Test
-	public void AddDatatoMyDB() {
+	@Test(priority = 1)
+	public void AddDatatoMyDB() throws SQLException {
+		
+		String Query = "INSERT INTO customers (customerNumber, customerName, contactLastName, contactFirstName, phone, addressLine1, city, country)\r\n"
+				+ "VALUES (6486, 'MaisJamil', 'Barhhoumeh', 'Mais', '123654789', '67, rue des Cinquante Otages', 'Amman', 'Jordan');";
+		stmt = con.createStatement();
+		stmt.executeUpdate(Query);
 		
 	}
+	
+	@Test(priority = 2)
+	public void updateData() throws SQLException {
+		
+		String Query = "update customers set contactFirstName='Maissss' where  customerNumber=6486";
+		stmt = con.createStatement();
+		stmt.executeUpdate(Query);
+	}
+	
+	@Test(priority = 3)
+	public void readData() throws SQLException {
+		String Query = "select * from customers where  customerNumber=6486";
+		stmt = con.createStatement();
+		rs = stmt.executeQuery(Query);
+		
+		while(rs.next()) {
+			String DBcustomerNum = rs.getString("customerNumber");
+			String DBcustomerName = rs.getString("contactFirstName");
+			
+			System.out.println(DBcustomerNum);
+			System.out.println(DBcustomerName);
+
+		}
+		
+	}
+	
+	@Test(priority = 4)
+	public void deleteData() throws SQLException {
+		
+		String Query = "delete from customers where customerNumber=6486";
+		stmt = con.createStatement();
+		stmt.executeUpdate(Query);
+				
+	}
 }
+
+
+
+
+
